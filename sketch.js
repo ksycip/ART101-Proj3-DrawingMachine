5390686a18439cbbd87916e46cbe6c7c62fc2f0c
ksycip <56284512+ksycip@users.noreply.github.com>
2020-11-03 22:57:48 -0800

    attempt to fix

--- sketch.js
+++ sketch.js
@@ -1,109 +1,76 @@
+// 900, 500
+
 let array = [];
 //let backgroundColor = (250, 233, 225);
 let strokeWidth = 5;
 
 let noiseOffset = 0.0;
+let colorPicker;
 
 
 function setup() {
-  createCanvas(600, 600);
-  //createCanvas(windowWidth, windowHeight);
-  //background(220);
+// createCanvas(900, 500);
+// ;
+  createCanvas(windowWidth, windowHeight);
+  // background(220);
+  // var cnv = createCanvas(windowWidth, windowHeight);
+  //   var x = (windowWidth - width) / 2;
+  //   var y = (windowHeight - height) / 2;
+  //   cnv.position(x, y);
 
+    // background(124, 181, 176);
 
+    //strokeWeight(10);
+    colorPicker = createColorPicker('');
+    colorPicker.position(5, 15);
 
-  var cnv = createCanvas(600, 600);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
-  cnv.position(x, y);
-
-  //background(250, 233, 225);
-  drawGrid();
-
-  //strokeWeight(10);
-
-  noFill();
+    noFill();
 }
 
 function draw() {
+  // strokeWeight(strokeWidth);
+  stroke(colorPicker.color());
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 
-  background(250, 233, 225, 5);
-  //strokeWeight(strokeWidth);
-
-  noiseOffset += 0.05;
-  strokeWidth = noise(noiseOffset) * 100;
-
-
-  // if (mouseIsPressed){
+  // noiseOffset += 0.05;
+  // strokeWidth = noise(noiseOffset) * 25;
   //
-  //   backgroundColor -= 5;
-  //   background(backgroundColor);
-  //
-  //
-  //   array.push([mouseX, mouseY]);
-
-  stroke(map(mouseX, 0, 600, 0, 255, true));
-  line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
-  line(mouseX, mouseY, pmouseX, pmouseY);
-
-  // beginShape();
-  // for(let i = 0; i < array.length; i++){
-  //   //line(array[i][0], array [i][1], array[i + 1][0], array[i + 1][1]);
-  //   curveVertex(array[i][0], array [i][1]);
-  //
-  //     }
-  // endShape();
-
+  // //stroke(map(mouseX, 214, 242, 255, true));
+  // line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+  // line(mouseX, mouseY,pmouseX, pmouseY);
 }
-//}
 
-function keyTyped() {
 
-  if (key === 's') {
+
+function keyTyped(){
+
+  if (key === 's'){
     //save this image
     saveCanvas('fileName', 'png');
   } else if (key === 'c') {
     //clear image
     clear();
 
-    // else if(key === 'd'){
-    //   //display image
-    //   background(250, 233, 225);
+  // else if(key === 'd'){
+  //   //display image
+  //   background(250, 233, 225);
     beginShape();
-    for (let i = 0; i < array.length; i++) {
+    for(let i = 0; i < array.length; i++){
       //line(array[i][0], array [i][1], array[i + 1][0], array[i + 1][1]);
-      curveVertex(array[i][0], array[i][1]);
-    }
-    endShape();
+      curveVertex(array[i][0], array [i][1]);
+        }
+  endShape();
     //line(array[0][0], array [0][1], array[1][0], array[1][1]);
     // line(array[1][0], array [1][1], array[2][0], array[2][1]);
   }
   return false;
-}
-
-
-function mousePressed() {
-  array = [];
-  backgroundColor = (250, 233, 225);
-
-}
-
-function drawGrid() {
-  numCells = 20;
-  fillColor = 255;
-    //noStroke();
-    strokeWeight(0);
-
-  for (let i = 0; i <= width; i += width / numCells) {
-    for (let j = 0; j <= height; j += height / numCells) {
-      if (fillColor === 255){
-        fillColor = 200;
-      } else {
-        fillColor = 255;
-      }
-      fill(fillColor);
-      rect(i, j, width / numCells, height / numCells);
     }
-  }
-  strokeWeight(strokeWidth);
-}
+
+
+// function mousePressed(){
+//   array = [];
+//   // backgroundColor = ();
+//
+// }
